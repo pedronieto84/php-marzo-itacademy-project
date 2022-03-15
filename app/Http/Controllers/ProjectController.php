669e3d9d807cab68d7e47086f4ed55eb8c6b86ef
enric <enric@labaseweb.com>
2022-03-15 11:15:07 +0100

    Recuperar dades json a ProjectController, no les sé passar a $project. Insertades funcions a UserController.

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -22,39 +22,56 @@ class ProjectController extends Controller
         return $response;
     }
 
-    public function show(Project $project){
+    public function show(Project $project)
+    {
         $project['techsets'] = $project->projectTechsetNames($project['id']);
         $project['files'] =  $project->projectFiles($project['id']);
         return $project;
         // return response()->json($project);
     }
 
-    public function create(Request $request){
-        $request->validate([
-            'title' => 'required|string',
-            'publishedDate' => 'required|date',
-            'deadline' => 'required|date',
-            'shortExplanation' => 'string|nullable'
-        ]);
+    public function create(Request $request)
+    {
+        // dd($request);
+        $json = file_get_contents('php://input');
+        $data=json_decode($json, true);
+        // dd($data);
+        // if (json_last_error() == JSON_ERROR_SYNTAX){
+        //     return "error json_error_syntax";
+        // }
+        // dd(json_last_error());
+        // $data->validate([
+        //     'title' => 'required|string',
+        //     'publishedDate' => 'required|date',
+        //     'deadline' => 'required|date',
+        //     'shortExplanation' => 'string|nullable'
+        // ]);
         $project = new Project;
-        $project->user_id = $request->user_id;
-        $project->title = $request->title;
-        $project->publishedDate = $request->publishedDate;
-        $project->deadline = $request->deadline;
-        // techsets i files s'han de rebre també pel request? Com?
-        // $project['techsets'] = $request->projectTechsetNames($request);
-        // $project->techsets = $request->techsets;
-        $project['techsets'] = $request['techsets'];
-        // $project->files = $request->files;
-        $project->shortExplanation = $request->shortExplanation;
-        $project->state = $request->state;
-        $project->bid = $request->bid;
+        $project->user_id = $data['user_id'];
+        $project->title = $data['title'];
+        $project->publishedDate = $data['publishedDate'];
+        $project->deadline = $data['deadline'];
+        $project->techSet = $data['techSet'];
+        $project->filesArray = $data['filesArray'];
+        $project->shortExplanation = $data['shortExplanation'];
+        $project->state = $data['state'];
+        $project->bid = $data['bid'];
+        // $project->user_id = $data->user_id;
+        // $project->title = $data->title;
+        // $project->publishedDate = $data->publishedDate;
+        // $project->deadline = $data->deadline;
+        // $project->techSet = $data->techSet;
+        // $project->filesArray = $data->filesArray;
+        // $project->shortExplanation = $data->shortExplanation;
+        // $project->state = $data->state;
+        // $project->bid = $data->bid;
         $project->save();
         return $project;
         // return response()->json($project);
     }
 
-    public function update(Request $request, Project $project){
+    public function update(Request $request, Project $project)
+    {
         // dd($project);
         // dd($request);
         $request->validate([
@@ -75,7 +92,8 @@ class ProjectController extends Controller
         // return response()->json($project);
     }
 
-    public function delete(Project $project){
+    public function delete(Project $project)
+    {
         $project->delete();
         return $project;
     }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,43 @@ class UserController extends Controller
     {
         return response()->json(User::allUsers());
     }
+
+    public function show(User $user){
+        return $user;
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([]);
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->verified = $request->verified;
+        $user->projectsPublished = $request->projectsPublished;
+        $user->admin = $request->admin;
+        $user->typeOfInstitution = $request->typeOfInstitution;
+        $user->save();
+        return $user;
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $request->validate([]);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->verified = $request->verified;
+        $user->projectsPublished = $request->projectsPublished;
+        $user->admin = $request->admin;
+        $user->typeOfInstitution = $request->typeOfInstitution;
+        $user->save();
+        return $user;
+    }
+
+    public function delete(User $user)
+    {
+        $user->delete();
+        return $user;
+    }
 }
