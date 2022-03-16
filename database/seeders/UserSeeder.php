2cc4a9f08d61ca755309fcbb7ce77f71f2ee6a7a
Alejandro Gallardo Baro <lexbcn@gmail.com>
2022-03-16 22:04:09 +0100

    Fix UserSeeder para los registros manuales de usuario

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory;
 
 class UserSeeder extends Seeder
 {
@@ -21,6 +22,27 @@ class UserSeeder extends Seeder
         User::factory()
             ->count(10)
             ->create();
+            $typeOfInstitution = ['Empresa Pública','ONG o empreses del 3er sector','Empresa Privada','Altres'][rand(0,3)];        
+            $faker = Factory::create();
+            $email = $faker->unique()->safeEmail();
+            User::create ([
+                'name' => $faker->name(),
+                'email' => 'admin@example.org',
+                'email_verified_at' => $faker->dateTimeBetween(new DateTime('-9 weeks'), new DateTime('-3 weeks')),
+                'password' => Hash::make('adm123'), // el password es el email sin el dominio
+                'admin' => true,
+                'remember_token' => Str::random(10),
+                'typeOfInstitution' => $typeOfInstitution
+            ]);            
+            User::create ([
+                'name' => $faker->name(),
+                'email' => 'noadmin@example.org',
+                'email_verified_at' => $faker->dateTimeBetween(new DateTime('-9 weeks'), new DateTime('-3 weeks')),
+                'password' => Hash::make('adm123'), // el password es el email sin el dominio
+                'admin' => false,
+                'remember_token' => Str::random(10),
+                'typeOfInstitution' => $typeOfInstitution
+            ]);            
     }
 }
 
